7c36af264beae61cf5ed4ee48c3b979ba728d70b
cozej4 <ilakozejumanne@gmail.com>
2024-05-10 16:16:54 +0300

    :hammer: Closes #7

--- opensrp-chw-ovc/src/main/java/org/smartregister/chw/ovc/actionhelper/MvcEducationAndPsychosocialSupportActionHelper.java
+++ opensrp-chw-ovc/src/main/java/org/smartregister/chw/ovc/actionhelper/MvcEducationAndPsychosocialSupportActionHelper.java
@@ -1,23 +1,66 @@
 package org.smartregister.chw.ovc.actionhelper;
 
+import static org.smartregister.client.utils.constants.JsonFormConstants.GLOBAL;
+
+import android.content.Context;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.smartregister.chw.ovc.domain.MemberObject;
+import org.smartregister.chw.ovc.domain.VisitDetail;
 import org.smartregister.chw.ovc.model.BaseOvcVisitAction;
 import org.smartregister.chw.ovc.util.JsonFormUtils;
+
+import java.util.List;
+import java.util.Map;
 
 import timber.log.Timber;
 
 public class MvcEducationAndPsychosocialSupportActionHelper extends OvcVisitActionHelper {
-    private String selectEducationVocationalTraining;
     private String selectEcdPsychosocialSupport;
+
+    private JSONObject jsonForm;
+
+    private final MemberObject memberObject;
+
+    public MvcEducationAndPsychosocialSupportActionHelper(MemberObject memberObject) {
+        this.memberObject = memberObject;
+    }
+
+    @Override
+    public void onJsonFormLoaded(String jsonString, Context context, Map<String, List<VisitDetail>> details) {
+        super.onJsonFormLoaded(jsonString, context, details);
+        try {
+            jsonForm = new JSONObject(jsonString);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    /**
+     * Update the preprocessed form to pass client age as global parameter
+     *
+     * @return the updated form
+     */
+    @Override
+    public String getPreProcessed() {
+        try {
+            JSONObject global = jsonForm.getJSONObject(GLOBAL);
+            global.put("age", memberObject.getAge());
+            return jsonForm.toString();
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
+        return null;
+    }
+
 
     @Override
     public void onPayloadReceived(String jsonPayload) {
         JSONObject payload;
         try {
             payload = new JSONObject(jsonPayload);
-            selectEducationVocationalTraining = JsonFormUtils.getValue(payload,"select_education_vocational_training");
-            selectEcdPsychosocialSupport = JsonFormUtils.getCheckBoxValue(payload,"select_ecd_psychosocial_support");
+            selectEcdPsychosocialSupport = JsonFormUtils.getCheckBoxValue(payload, "ecd_psychosocial_support");
         } catch (JSONException e) {
             Timber.d(e);
         }
@@ -25,7 +68,7 @@ public class MvcEducationAndPsychosocialSupportActionHelper extends OvcVisitActi
 
     @Override
     public String evaluateSubTitle() {
-        if(selectEcdPsychosocialSupport != null){
+        if (selectEcdPsychosocialSupport != null) {
             return null;
         } else {
             return null;
@@ -34,7 +77,7 @@ public class MvcEducationAndPsychosocialSupportActionHelper extends OvcVisitActi
 
     @Override
     public BaseOvcVisitAction.Status evaluateStatusOnPayload() {
-        if(selectEcdPsychosocialSupport != null){
+        if (selectEcdPsychosocialSupport != null) {
             return BaseOvcVisitAction.Status.COMPLETED;
         } else {
             return BaseOvcVisitAction.Status.PENDING;
